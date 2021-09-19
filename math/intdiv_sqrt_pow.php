ec38b6495d40f86eaf1c3d210d9cf1f0c2cbbf65
Faisal Ahammad <faisal13108@gmail.com>
2021-09-20 03:18:08 +0600

    PHP Math Intdiv, Sqrt, Pow

---
+++ math/intdiv_sqrt_pow.php
@@ -0,0 +1,18 @@
+<?php
+/*-------intdiv() -- Integer division------- */
+echo intdiv(100, 3) . "\n";
+echo intdiv(5, 2) . "\n";
+echo intdiv(-5, -2) . "\n";
+
+/*-------sqrt() -- Square root------- */
+echo (sqrt(9) . "\n");
+echo (sqrt(4) . "\n");
+echo (sqrt(0) . "\n");
+echo (sqrt(144) . "\n");
+echo (sqrt(5) . "\n");
+
+/*-------pow() -- Exponential expression------- */
+echo (pow(2, 4) . "\n");
+echo (pow(2, 2) . "\n");
+echo (pow(-2, 4) . "\n");
+echo (pow(-2, -4) . "\n");
