0a04eeeb74c6571c6360ccc41e449ab181f2379e
Faisal Ahammad <faisal13108@gmail.com>
2021-09-20 03:10:58 +0600

    PHP Math Ceil, Floor, Round, Abs

---
+++ math/ceil_floor_round_abs.php
@@ -0,0 +1,32 @@
+<?php
+/*-------Math:Ceil Function------- */
+echo (ceil(0.10) . "\n");
+echo (ceil(0.60) . "\n");
+echo (ceil(5) . "\n");
+echo (ceil(5.1) . "\n");
+echo (ceil(-5.1) . "\n");
+echo (ceil(-5.9) . "\n");
+
+/*-------Math:Floor Function------- */
+echo (floor(0.60) . "\n");
+echo (floor(0.40) . "\n");
+echo (floor(5) . "\n");
+echo (floor(5.1) . "\n");
+echo (floor(-5.1) . "\n");
+echo (floor(-5.9) . "\n");
+
+/*-------Math:Round Function------- */
+echo (round(0.60) . "\n");
+echo (round(0.40) . "\n");
+echo (round(5) . "\n");
+echo (round(5.1) . "\n");
+echo (round(-5.1) . "\n");
+echo (round(-5.9) . "\n");
+
+/*-------Math:Abs Function------- */
+echo (abs(0.60) . "\n");
+echo (abs(0.50) . "\n");
+echo (abs(5) . "\n");
+echo (abs(5.1) . "\n");
+echo (abs(-5.1) . "\n");
+echo (abs(-5.91));
