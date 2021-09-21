8572132a39626ed79e3a04de50f610c1992e1ac7
Faisal Ahammad <faisal13108@gmail.com>
2021-09-21 15:49:46 +0600

    PHP Date

---
+++ math/date.php
@@ -0,0 +1,41 @@
+<?php
+/*----Date Functions------- */
+echo "Today is " . date("d") . "\n";
+
+echo "Today is " . date("j") . "\n";
+
+echo "Today is " . date("jS") . "\n";
+
+echo "Month is " . date("F") . "\n";
+
+echo "Month is " . date("m") . "\n";
+
+echo "Month is " . date("n") . "\n";
+
+echo "Month is " . date("M") . "\n";
+
+echo "Year is " . date("Y") . "\n";
+
+echo "Year is " . date("y") . "\n";
+
+echo "Full Date is " . date("d/m/Y") . "\n";
+
+echo "Full Date is " . date("Y-m-d") . "\n";
+
+echo "Full Date is " . date("Y-M-d") . "\n";
+
+echo "Week Day is " . date("D") . "\n";
+
+echo "Week Day is " . date("l") . "\n";
+
+echo "Week Day is " . date("N") . "\n";
+
+echo "Week Day is " . date("w") . "\n";
+
+echo "Day is " . date("z") . "\n";
+
+echo "Week of the year " . date("W") . "\n";
+
+echo "Days of the Month " . date("t") . "\n";
+
+echo "Is this is Leap year" . date("L") . "\n";
