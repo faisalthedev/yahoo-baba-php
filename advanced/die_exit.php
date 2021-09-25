6882a6ae5061682ee992b88df7c10664cd0d55a7
Faisal Ahammad <faisal13108@gmail.com>
2021-09-26 00:28:45 +0600

    PHP Die & Exit

---
+++ advanced/die_exit.php
@@ -0,0 +1,16 @@
+<?php
+/* ----------Die Function ----------*/
+echo "1. Some Message \n";
+echo "2. Some Message \n";
+echo "3. Some Message \n";
+die("Here is some error!!!");
+echo "4. Some Message \n";
+echo "5. Some Message \n";
+
+/* ----------Exit Function ----------*/
+// echo "1. Some Message \n";
+// echo "2. Some Message \n";
+// echo "3. Some Message \n";
+// exit("Here is some error!!!");
+// echo "4. Some Message \n";
+// echo "5. Some Message \n";
