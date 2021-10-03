7c62d799e5bbff9d8ba12ddad61c0ca868a08292
Faisal Ahammad <faisal13108@gmail.com>
2021-10-03 22:27:23 +0600

    Delete by ID option added and ready to deploy

--- mysql_crud/delete.php
+++ mysql_crud/delete.php
@@ -1,9 +1,25 @@
-<?php include 'header.php'; ?>
+<?php
+include 'header.php';
+
+if (isset($_POST['deletebtn'])) {
+    $stu_id = $_POST['sid'];
+
+    // include config
+    include 'config.php';
+    $sql = "DELETE FROM student WHERE sid={$stu_id}";
+    $result = mysqli_query($conn, $sql) or die('Query uncessful!');
+
+    // redirect to the home page
+    header("Location: http://localhost/yahoo-baba-php/mysql_crud/index.php");
+
+    mysqli_close($conn);
+}
+?>
 
 
 <div id="main-content">
     <h2>Delete Record</h2>
-    <form class="post-form" action="" method="post">
+    <form class="post-form" action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
         <div class="form-group">
             <label>Id</label>
             <input type="text" name="sid" />
@@ -13,4 +29,5 @@
 </div>
 </div>
 </body>
+
 </html>
