28910bd24d8c06ca872cb071d73fe22050d57e50
Faisal Ahammad <faisal13108@gmail.com>
2021-10-04 02:18:35 +0600

    User delete option added

---
+++ yb-blog/admin/delete-user.php
@@ -0,0 +1,14 @@
+<?php
+include 'config.php';
+
+$userid = $_GET['id'];
+
+$sql = "DELETE FROM user WHERE user_id='{$userid}'";
+
+if (mysqli_query($conn, $sql)) {
+  header("Location: {$hostname}/admin/users.php");
+} else {
+  echo "<p class='form-warning'>Can't delete the user record.</p>";
+}
+
+mysqli_close($conn);
