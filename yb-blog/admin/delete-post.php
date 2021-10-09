90ffa4bbce2812e3c805bb746d9ed28bede4d1c1
Faisal Ahammad <faisal13108@gmail.com>
2021-10-09 20:05:44 +0600

    PHP News Project - Delete Image from Folder

--- yb-blog/admin/delete-post.php
+++ yb-blog/admin/delete-post.php
@@ -4,6 +4,13 @@ include "config.php";
 $post_id = $_GET['id'];
 $cat_id = $_GET['catid'];
 
+$sql1 = "SELECT * FROM post WHERE post_id={$post_id}";
+$result = mysqli_query($conn, $sql1) or die("Query Failed: Select");
+$row = mysqli_fetch_assoc($result);
+
+// delete image
+unlink("upload/" . $row['post_img']);
+
 $sql = "DELETE FROM post WHERE post_id={$post_id};";
 $sql .= "UPDATE category SET post=post-1 WHERE category_id={$cat_id}";
 
