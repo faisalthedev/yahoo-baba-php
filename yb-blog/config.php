77612b6b534ef552a4c4f264a2bca3a716dc4429
Faisal Ahammad <faisal13108@gmail.com>
2021-10-10 18:21:48 +0600

    PHP News Project - Dynamic Front & Single Page

---
+++ yb-blog/config.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', 'root', 'news-site') or die("Connection failed!" . mysqli_connect_error());
+$hostname = "http://localhost/yahoo-baba-php/yb-blog";
